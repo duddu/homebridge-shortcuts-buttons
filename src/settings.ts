f0fe26c39b88e70e0cc5bfa68c031c187f7b0af7
Davide Doronzo <davide.doronzo@gmail.com>
2024-04-23 18:42:00 +0100

    refactor(settings): read values from package json

--- src/settings.ts
+++ src/settings.ts
@@ -1,15 +1,17 @@
+import { displayName, name } from './../package.json';
+
 /**
  * This is the name of the platform that users will use to register the plugin in the
  * Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomebridgeShortcutsButtons';
+export const PLATFORM_NAME = displayName.replace(/\s/g, '');
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-shortcuts-buttons';
+export const PLUGIN_NAME = name;
 
 /**
- * This plugin uses a single device/accessory just as a wrapper for the services
+ * This plugin uses a single device just as a wrapper for the buttons services
  */
 export const DEVICE_SERIAL = '634EA867A81D59F1898';
