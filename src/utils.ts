747526e17db5cc3ad534922c5d77a30a5e6a2d90
Davide Doronzo <davide.doronzo@gmail.com>
2024-04-23 18:48:42 +0100

    refactor(utils): add non-empty strings validator method

--- src/utils.ts
+++ src/utils.ts
@@ -3,15 +3,21 @@ import { ObjectEncodingOptions } from 'fs';
 import { Logger } from 'homebridge';
 import { promisify } from 'util';
 
+const EXEC_DEFAULT_TIMEOUT = 5000;
+
 export class HSBUtils {
   constructor(private readonly log: Logger) {}
 
   public async execAsync(
     command: string,
-    options?: ObjectEncodingOptions & ExecOptions,
+    options: ObjectEncodingOptions & ExecOptions = { timeout: EXEC_DEFAULT_TIMEOUT },
   ): Promise<void> {
     const { stdout, stderr } = await promisify(exec).call(null, command, options);
-    stdout.length > 0 && this.log.debug(stdout.toString());
-    stderr.length > 0 && this.log.error(stderr.toString());
+    this.isNonEmptyString(stdout.toString()) && this.log.debug(stdout.toString());
+    this.isNonEmptyString(stderr.toString()) && this.log.error(stderr.toString());
+  }
+
+  public isNonEmptyString(str: unknown): str is string {
+    return typeof str === 'string' && str.trim() !== '';
   }
 }
