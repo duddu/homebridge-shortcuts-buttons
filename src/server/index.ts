7170ab4fcb125bdcde558e6dfb738a3a4b97d304
Davide Doronzo <davide.doronzo@gmail.com>
2024-04-27 10:49:57 +0100

    refactor(server): moved to dedicated directory and split

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,13 @@
 import { API, Logger } from 'homebridge';
 import { createServer, IncomingMessage, Server, ServerResponse } from 'http';
 import { Socket } from 'net';
-import { join } from 'path';
-import { URLSearchParams } from 'url';
 
-import { HSBConfig } from './config';
-import { PLATFORM_NAME } from './settings';
-import { HSBShortcutStatus } from './shortcut';
-import { HSBUtils } from './utils';
+import { HSBXCallbackUrlServerCommand } from './command';
+import { HSBConfig } from '../config';
+import { HSBXCallbackUrlSearchParams } from './params';
+import { PLATFORM_NAME } from '../settings';
+import { HSBUtils } from '../utils';
+import { createRequestValidators } from './validators';
 
 export class HSBXCallbackUrlServer {
   private readonly pathname = '/x-callback-url';
@@ -18,6 +18,7 @@ export class HSBXCallbackUrlServer {
   private readonly hostname: string;
   private readonly port: number;
   private readonly server: Server;
+  private readonly command: HSBXCallbackUrlServerCommand;
 
   constructor(
     private readonly config: HSBConfig,
@@ -30,6 +31,8 @@ export class HSBXCallbackUrlServer {
     this.port = config.callbackServerPort;
 
     this.server = this.create();
+
+    this.command = new HSBXCallbackUrlServerCommand(this.config, this.utils);
 
     api.on('shutdown', this.destroy);
   }
@@ -109,7 +112,7 @@ export class HSBXCallbackUrlServer {
       this.utils,
     );
 
-    const requestValidators = this.createRequestValidators({
+    const requestValidators = createRequestValidators({
       isSupported: {
         condition: () => typeof url === 'string' || method === 'GET',
         errorMessage: `Unsupported request: ${method}:${url}`,
@@ -141,7 +144,7 @@ export class HSBXCallbackUrlServer {
     this.log.debug('XCallbackUrlServer::requestListener Request validators passed');
 
     try {
-      await this.runCallbackCommand(searchParams);
+      await this.command.run(searchParams);
     } catch (e) {
       return this.endWithError(res, 500, 'Failed to run callback command', e);
     }
@@ -153,15 +156,6 @@ export class HSBXCallbackUrlServer {
 
     return this.endWithStatusAndHtml(res, 200);
   };
-
-  private createRequestValidators(
-    validatorsMap: HSBXCallbackUrlRequestValidatorsMap,
-  ): HSBXCallbackUrlRequestValidator[] {
-    return Object.values(validatorsMap).map(
-      ({ condition, errorCode, errorMessage }) =>
-        new HSBXCallbackUrlRequestValidator(condition, errorCode, errorMessage),
-    );
-  }
 
   private endWithError(
     res: ServerResponse,
@@ -182,134 +176,7 @@ export class HSBXCallbackUrlServer {
     res.write(CALLBACK_HTML_CONTENT);
     res.end();
   }
-
-  private async runCallbackCommand(searchParams: HSBXCallbackUrlSearchParamsType): Promise<void> {
-    let command = this.config.callbackCustomCommand;
-
-    if (!this.utils.isNonEmptyString(command)) {
-      command = this.getDefaultCallbackCommand(searchParams);
-    }
-
-    await this.utils.execAsync(command, {
-      env: {
-        SHORTCUT_NAME: searchParams.shortcut,
-        SHORTCUT_STATUS: searchParams.status,
-        SHORTCUT_RESULT: searchParams.result,
-        SHORTCUT_ERROR: searchParams.errorMessage,
-      },
-      timeout: this.config.callbackCommandTimeout,
-    });
-  }
-
-  private getDefaultCallbackCommand(searchParams: HSBXCallbackUrlSearchParamsType): string {
-    let subtitle: string;
-    let sound: string;
-    switch (searchParams.status) {
-      case HSBShortcutStatus.SUCCESS:
-        subtitle = 'executed successfully';
-        searchParams.result && (subtitle += `\nResult: ${searchParams.result}`);
-        sound = 'Glass';
-        break;
-      case HSBShortcutStatus.ERROR:
-        subtitle = 'execution failed';
-        searchParams.errorMessage && (subtitle += `\nError: ${searchParams.errorMessage}`);
-        sound = 'Sosumi';
-        break;
-      case HSBShortcutStatus.CANCEL:
-        subtitle = 'execution was cancelled';
-        sound = 'Sosumi';
-        break;
-      default:
-        subtitle = 'received an unknown result status';
-        sound = 'Sosumi';
-        break;
-    }
-
-    return (
-      `open ${this.defaultCallbackCommandAppPath} ` +
-      `--env NOTIFICATION_TITLE="${this.config.name}" ` +
-      `--env NOTIFICATION_SUBTITLE="${searchParams.shortcut} ${subtitle}" ` +
-      `--env NOTIFICATION_SOUND="${sound}"`
-    );
-  }
-
-  private readonly defaultCallbackCommandAppPath = join(
-    __dirname,
-    './bin/HomebridgeShortcutsButtons\\ -\\ Notify\\ Shortcut\\ Result.app',
-  );
 }
-
-enum HSBXCallbackUrlRequiredSearchParamsKeys {
-  SHORTCUT = 'shortcut',
-  STATUS = 'status',
-  TOKEN = 'token',
-}
-
-enum HSBXCallbackUrlOptionalSearchParamsKeys {
-  ERROR_MESSAGE = 'errorMessage',
-  RESULT = 'result',
-}
-
-const requiredParamsKeysList = Object.values(HSBXCallbackUrlRequiredSearchParamsKeys);
-const optionalParamsKeysList = Object.values(HSBXCallbackUrlOptionalSearchParamsKeys);
-
-type HSBXCallbackUrlSearchParamsType = {
-  [K in HSBXCallbackUrlRequiredSearchParamsKeys]: string;
-} & {
-  [K in HSBXCallbackUrlOptionalSearchParamsKeys]: string | undefined;
-};
-
-class HSBXCallbackUrlSearchParams implements HSBXCallbackUrlSearchParamsType {
-  public readonly shortcut!: string;
-  public readonly status!: string;
-  public readonly token!: string;
-  public readonly result: string | undefined;
-  public readonly errorMessage: string | undefined;
-
-  constructor(
-    searchParams: URLSearchParams,
-    private readonly utils: HSBUtils,
-  ) {
-    for (const key of requiredParamsKeysList) {
-      this[key] = searchParams.get(key) || '';
-    }
-    for (const key of optionalParamsKeysList) {
-      const value = searchParams.get(key);
-      this[key] = utils.isNonEmptyString(value) ? value : undefined;
-    }
-  }
-
-  public areValidRequiredParamsValues = (): boolean => {
-    return requiredParamsKeysList.every((key) => this.utils.isNonEmptyString(this[key]));
-  };
-}
-
-interface HSBXCallbackUrlRequestValidatorMembersBase {
-  errorCode: number;
-  errorMessage: string;
-}
-
-class HSBXCallbackUrlRequestValidator implements HSBXCallbackUrlRequestValidatorMembersBase {
-  constructor(
-    private readonly condition: () => boolean,
-    public readonly errorCode: number,
-    public readonly errorMessage: string,
-  ) {}
-
-  public get passed(): boolean {
-    try {
-      return this.condition() === true;
-    } catch (e) {
-      return false;
-    }
-  }
-}
-
-type HSBXCallbackUrlRequestValidatorsMap = {
-  [K: string]: HSBXCallbackUrlRequestValidatorMembersBase & {
-    condition: HSBXCallbackUrlRequestValidator['condition'];
-  };
-};
 
 const CALLBACK_HTML_CONTENT = `<!DOCTYPE html>
 <html class="default" lang="en">
