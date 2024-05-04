c7d3136f6014841a65345490d65b6cb35e5cfb0c
Davide Doronzo <davide.doronzo@gmail.com>
2024-05-04 16:22:31 +0100

    refactor(accessory): methods cleanup and renaming

--- src/accessory.ts
+++ src/accessory.ts
@@ -1,58 +1,56 @@
-import { Characteristic, PlatformAccessory, Service } from 'homebridge';
+import { PlatformAccessory } from 'homebridge';
 
+import { HSBConfig } from './config';
 import { HSBPlatform } from './platform';
 import { HSBService, HSBServiceType } from './service';
 import { PLATFORM_NAME } from './settings';
-import { HSBConfig } from './config';
-
-export type HSBPlatformAccessory = PlatformAccessory<HSBAccessoryContext>;
 
 export interface HSBAccessoryContext {
   device: HSBDevice;
 }
 
-export class HSBAccessory {
-  private readonly Service: typeof Service;
-  private readonly Characteristic: typeof Characteristic;
-  private readonly serviceType: HSBServiceType;
+export type HSBPlatformAccessory = PlatformAccessory<HSBAccessoryContext>;
 
+export class HSBAccessory {
   constructor(
     private readonly platform: HSBPlatform,
-    private readonly accessory: HSBPlatformAccessory,
+    private readonly platformAccessory: HSBPlatformAccessory,
   ) {
-    this.Service = this.platform.api.hap.Service;
-    this.Characteristic = this.platform.api.hap.Characteristic;
-    this.serviceType = this.getShortcutsServiceType();
-
     this.addAccessoryInformationService();
     this.addShortcutsServices();
   }
 
   private addAccessoryInformationService() {
-    (
-      this.accessory.getService(this.Service.AccessoryInformation) ??
-      this.accessory.addService(this.Service.AccessoryInformation)
-    )
-      .setCharacteristic(this.Characteristic.Manufacturer, 'Homebridge.io')
-      .setCharacteristic(this.Characteristic.Model, PLATFORM_NAME)
+    this.platformAccessory
+      .getService(this.platform.api.hap.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.api.hap.Characteristic.Manufacturer, 'Homebridge.io')
+      .setCharacteristic(this.platform.api.hap.Characteristic.Model, PLATFORM_NAME)
       .setCharacteristic(
-        this.Characteristic.SerialNumber,
-        this.accessory.context.device.serialNumber,
+        this.platform.api.hap.Characteristic.SerialNumber,
+        this.platformAccessory.context.device.serialNumber,
       );
   }
 
   private addShortcutsServices() {
+    const serviceType: HSBServiceType =
+      this.platform.api.hap.Service[this.platform.config.serviceType];
     const activeServicesSubtypes: Set<string | undefined> = new Set();
 
     for (const serviceConfig of this.platform.config.services) {
       const subtype = this.platform.api.hap.uuid.generate(JSON.stringify(serviceConfig));
 
-      let service = this.accessory.getServiceById(this.serviceType, subtype);
-      let logServiceOrigin: string = 'Restored from cache';
+      let service = this.platformAccessory.getServiceById(serviceType, subtype);
+      let logServiceOrigin = 'Restored from cache';
+
       if (!service) {
-        service = this.accessory.addService(this.serviceType, serviceConfig.serviceName, subtype);
+        service = this.platformAccessory.addService(
+          serviceType,
+          serviceConfig.serviceName,
+          subtype,
+        );
         logServiceOrigin = 'Created from fresh config';
       }
+
       this.platform.log.debug(
         'Accessory::addShortcutsServices',
         `Service(${service.displayName})`,
@@ -67,30 +65,20 @@ export class HSBAccessory {
         serviceConfig,
         this.platform.server,
         this.platform.utils,
-        this.Characteristic,
+        this.platform.api.hap.Characteristic,
       );
     }
 
-    for (const service of this.accessory.services) {
+    for (const service of this.platformAccessory.services) {
       if (typeof service.subtype === 'string' && !activeServicesSubtypes.has(service.subtype)) {
-        this.accessory.removeService(service);
+        this.platformAccessory.removeService(service);
+
         this.platform.log.debug(
           'Accessory::addShortcutsServices',
           `Service(${service.displayName})`,
           'Removed as outdated',
         );
       }
-    }
-  }
-
-  private getShortcutsServiceType(): HSBServiceType {
-    switch (this.platform.config.serviceType) {
-      case 'Outlet':
-        return this.Service.Outlet;
-      case 'Switch':
-        return this.Service.Switch;
-      default:
-        return this.Service.Outlet;
     }
   }
 }
