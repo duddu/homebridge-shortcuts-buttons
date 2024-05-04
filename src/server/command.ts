bc5dee1a8b457dd6be471b637db73edc760f034e
Davide Doronzo <davide.doronzo@gmail.com>
2024-05-04 11:13:45 +0100

    docs(typedoc): generate documentation

--- src/server/command.ts
+++ src/server/command.ts
@@ -1,3 +1,7 @@
+/**
+ * @module server
+ */
+
 import { join } from 'path';
 
 import { HSBConfig } from '../config';
--- src/server/params.ts
+++ src/server/params.ts
@@ -1,3 +1,7 @@
+/**
+ * @module server
+ */
+
 import { URLSearchParams } from 'url';
 
 import { HSBShortcutStatus } from '../shortcut';
