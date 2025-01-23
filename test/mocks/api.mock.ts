2b380315c6c7eeb7fa31c4924cac757862d68812
Davide Doronzo <davide.doronzo@gmail.com>
2025-01-23 14:20:13 +0000

    test(api): add missing updateDisplayName implementation in platform accessory mock class

--- test/mocks/api.mock.ts
+++ test/mocks/api.mock.ts
@@ -6,27 +6,34 @@ import { generate } from 'hap-nodejs/dist/lib/util/uuid';
 import { HSBAccessoryContext, HSBDevice, HSBPlatformAccessory } from '../../src/accessory';
 import { HSBConfig } from '../../src/config';
 
+class HSBPlatformAccessoryMock extends Accessory implements HSBPlatformAccessory {
+  _associatedHAPAccessory: Accessory;
+  context: HSBAccessoryContext;
+
+  constructor(displayName: string, uuid: string) {
+    hbApiMockedPlatformAccessoryConstructorSpy(displayName, uuid);
+    super(displayName, uuid);
+    this._associatedHAPAccessory = this;
+    this.context = {
+      device: new HSBDevice({ accessoryName: displayName } as HSBConfig),
+    } as HSBAccessoryContext;
+  }
+
+  override emit = jest.fn<(event: unknown, port?: unknown, address?: unknown) => true>();
+
+  updateDisplayName(): void {
+    throw new Error(`Method "${this.updateDisplayName.name}" not implemented`);
+  }
+}
+
 export const hbApiMockedPlatformAccessoryConstructorSpy = jest.fn();
 
 class HBApiMock extends EventEmitter {
   public updatePlatformAccessories = jest.fn();
+
   public registerPlatformAccessories = jest.fn();
 
-  public platformAccessory = class extends Accessory implements HSBPlatformAccessory {
-    _associatedHAPAccessory: Accessory;
-    context: HSBAccessoryContext;
-
-    constructor(displayName: string, uuid: string) {
-      hbApiMockedPlatformAccessoryConstructorSpy(displayName, uuid);
-      super(displayName, uuid);
-      this._associatedHAPAccessory = this;
-      this.context = {
-        device: new HSBDevice({ accessoryName: displayName } as HSBConfig),
-      } as HSBAccessoryContext;
-    }
-
-    override emit = jest.fn<(event: unknown, port?: unknown, address?: unknown) => true>();
-  };
+  public platformAccessory = HSBPlatformAccessoryMock;
 
   public readonly hap = {
     uuid: {
